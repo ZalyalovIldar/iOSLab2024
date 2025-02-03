370940206c8e687724c538dc974bd5e7eb8501c8
Anna Vasilieva <56089819+anntvas@users.noreply.github.com>
2025-02-03 16:42:25 +0300

    entities

---
+++ MovieApplication/NSModel/MovieEntity+CoreDataClass.swift
@@ -0,0 +1,7 @@
+import Foundation
+import CoreData
+
+@objc(MovieEntity)
+public class MovieEntity: NSManagedObject {
+
+}
---
+++ MovieApplication/NSModel/MovieEntity+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+import Foundation
+import CoreData
+
+
+extension MovieEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<MovieEntity> {
+        return NSFetchRequest<MovieEntity>(entityName: "MovieEntity")
+    }
+
+    @NSManaged public var id: Int64
+    @NSManaged public var imagePath: Data?
+    @NSManaged public var title: String?
+
+}
+
+extension MovieEntity : Identifiable {
+
+}
